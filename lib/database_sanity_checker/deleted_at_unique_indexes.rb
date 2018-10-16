e798c473972c8d25f9c381ab4d90e574e0534cc9
Carlos Rojas <carlos.sadhu@gmail.com>
2018-10-16 11:36:33 +1100

    Use ActiveRecord::Base instead of ApplicationRecord

--- lib/database_sanity_checker/deleted_at_unique_indexes.rb
+++ lib/database_sanity_checker/deleted_at_unique_indexes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 def unique_indexes(model)
-  ApplicationRecord.connection.indexes(model.table_name).select(&:unique)
+  ActiveRecord::Base.connection.indexes(model.table_name).select(&:unique)
 end
 
 RSpec.shared_examples 'Unique indexes do not include deleted_at', :aggregate_failures do
   specify do
-    ApplicationRecord.descendants.each do |model|
+    ActiveRecord::Base.descendants.each do |model|
       unique_indexes(model).each do |unique_index|
         expect(unique_index.columns)
           .not_to include('deleted_at'),
